492537f29740c324cc1bed8c7d9bf03941e74c95
olga <olga-p-98@mail.ru>
2019-10-03 13:10:53 +0300

    some changes

--- bm25_search/bm25_search.py
+++ bm25_search/bm25_search.py
@@ -1,5 +1,4 @@
 import sys
-import pandas as pd
 import numpy as np
 from collections import Counter
 from collections import defaultdict
@@ -63,8 +62,10 @@ class SearchEngineBM25():
         return list(zip(self.texts[indices], result[indices]))
 
 
-with open("BMSearchEngine.pkl", "rb") as file:
-    BMSearchEngine = pickle.load(file)
+with open("docs.txt") as file:
+    docs = file.readlines()
+
+BMSearchEngine = SearchEngineBM25(docs)
 
 if __name__ == "__main__":
     for result in BMSearchEngine.search_matmul(sys.argv[1]):
